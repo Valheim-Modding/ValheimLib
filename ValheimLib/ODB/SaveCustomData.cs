dbc8d65a92b2944f14c997a34760ed3334eae42e
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-03-03 23:27:38 +0100

    Custom item data save system - Serialization to a file in case the user play the game without having the custom item installed and don't want to see them gone when installing the mods back

--- ValheimLib/ODB/SaveCustomData.cs
+++ ValheimLib/ODB/SaveCustomData.cs
@@ -1,35 +1,72 @@
-﻿namespace ValheimLib.ODB
+﻿using System.IO;
+using System.Linq;
+using UnityEngine.Assertions;
+
+namespace ValheimLib.ODB
 {
     internal static class SaveCustomData
     {
-		internal static void Init()
+        internal const string PlayerPrefix = "player_";
+        internal const string EntrySeparator = ".";
+
+        internal static readonly char[] InvalidFileNameChars = Path.GetInvalidFileNameChars();
+
+        internal static void Init()
         {
-			On.Inventory.Save += SaveModdedItems;
-			//On.Inventory.AddItem_string_int_float_Vector2i_bool_int_int_long_string += CheckIfModdedItem;
+            Directory.CreateDirectory(Util.Paths.CustomItemDataFolder);
+
+            On.Container.Awake += AddToCache;
+            On.Container.OnDestroyed += RemoveFromCache;
+
+            On.Inventory.Save += SaveModdedItems;
+            On.Inventory.Load += AddBackCustomItems;
 		}
+
+        private static void AddToCache(On.Container.orig_Awake orig, Container self)
+        {
+            orig(self);
+
+            Prefab.Cache.InventoryToContainer.Add(self.m_inventory, self);
+        }
+
+        private static void RemoveFromCache(On.Container.orig_OnDestroyed orig, Container self)
+        {
+            orig(self);
+
+            Prefab.Cache.InventoryToContainer.Remove(self.m_inventory);
+        }
+
+        private static string GetContainerUID(this Container container) => container.m_nview.GetZDO().m_uid.ToString();
+
+        internal static string GetInventoryUID(this Inventory self)
+        {
+            var localPlayer = Player.m_localPlayer;
+            if (localPlayer && localPlayer.m_inventory == self)
+            {
+                return PlayerPrefix + Game.instance.m_playerProfile.m_filename;
+            }
+            else
+            {
+                if (Prefab.Cache.InventoryToContainer.TryGetValue(self, out var container))
+                {
+                    return new string(container.GetContainerUID().Where(c => !InvalidFileNameChars.Contains(c)).ToArray());
+                }
+            }
+
+            return null;
+        }
 
         private static void SaveModdedItems(On.Inventory.orig_Save orig, Inventory self, ZPackage pkg)
         {
             orig(self, pkg);
 
-            var localPlayer = Player.m_localPlayer;
+            string inventoryId = self.GetInventoryUID();
+            Assert.IsNotNull(inventoryId);
 
-            string inventoryOwner = "";
-
-            if (localPlayer.m_inventory == self)
+            var inventoryFilePath = Path.Combine(Util.Paths.CustomItemDataFolder, inventoryId);
+            if (File.Exists(inventoryFilePath) && self.HasEveryItemFromSavedFile())
             {
-                inventoryOwner = localPlayer.m_name;
-            }
-            else
-            {
-                var containers = Prefab.Cache.GetPrefabs(typeof(Container));
-                foreach (Container container in containers.Values)
-                {
-                    if (container.m_inventory == self)
-                    {
-                        inventoryOwner = container.m_name;
-                    }
-                }
+                File.Delete(inventoryFilePath);
             }
 
             foreach (var itemData in self.m_inventory)
@@ -38,9 +75,10 @@
                 {
                     foreach (var customItem in ObjectDBHelper.CustomItems)
                     {
-                        if (customItem.ItemDrop.TokenName() == itemData.m_shared.m_name)
+                        customItem.ItemDrop.m_itemData.RemoveItemFromFile(inventoryId);
+                        if (customItem.ItemDrop.TokenName() == itemData.TokenName())
                         {
-                            SaveCustomItemToFile(inventoryOwner, itemData);
+                            itemData.SaveToFile(inventoryId);
 
                             break;
                         }
@@ -49,27 +87,27 @@
             }
         }
 
-        /*private static bool CheckIfModdedItem(On.Inventory.orig_AddItem_string_int_float_Vector2i_bool_int_int_long_string orig,
-            Inventory self, string name, int stack, float durability, Vector2i pos, bool equiped, int quality, int variant, long crafterID, string crafterName)
+        private static void AddBackCustomItems(On.Inventory.orig_Load orig, Inventory self, ZPackage pkg)
         {
-            var itemPrefab = ObjectDB.instance.GetItemPrefab(name);
-            if (itemPrefab == null)
+            orig(self, pkg);
+
+            string inventoryId = self.GetInventoryUID();
+
+            if (inventoryId != null)
             {
+                var inventoryFilePath = Path.Combine(Util.Paths.CustomItemDataFolder, inventoryId);
+                var hasDataForThisInventory = File.Exists(inventoryFilePath);
+                if (hasDataForThisInventory)
+                {
+                    if (self.HasEveryItemFromSavedFile())
+                    {
+                        File.Delete(inventoryFilePath);
+                        return;
+                    }
 
+                    self.AddCustomItemsFromFile(inventoryId);
+                }
             }
-        }*/
-
-        internal static void SaveCustomItemToFile(string inventoryOwner, ItemDrop.ItemData itemData)
-        {
-			/*pkg.Write(itemData.m_dropPrefab.name);
-			pkg.Write(itemData.m_stack);
-			pkg.Write(itemData.m_durability);
-			pkg.Write(itemData.m_gridPos);
-			pkg.Write(itemData.m_equiped);
-			pkg.Write(itemData.m_quality);
-			pkg.Write(itemData.m_variant);
-			pkg.Write(itemData.m_crafterID);
-			pkg.Write(itemData.m_crafterName);*/
         }
     }
 }
