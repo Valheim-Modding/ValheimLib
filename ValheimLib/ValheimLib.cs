d1f008ddcea5a691d81a3cb6f7d7baf96cde1884
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-03-06 22:23:09 +0100

    bump ver

--- ValheimLib/ValheimLib.cs
+++ ValheimLib/ValheimLib.cs
@@ -7,7 +7,7 @@ namespace ValheimLib
     {
         public const string ModGuid = "ValheimModdingTeam." + ModName;
         public const string ModName = "ValheimLib";
-        public const string ModVer = "0.0.4";
+        public const string ModVer = "0.0.5";
 
         internal static ValheimLib Instance { get; private set; }
 
