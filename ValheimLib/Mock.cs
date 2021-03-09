c49a3fa82b5af0e7dea93538f25f5f3ccf763a9e
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-03-09 21:50:45 +0100

    Use generic for mock

---
+++ ValheimLib/Mock.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityObject = UnityEngine.Object;
+
+namespace ValheimLib
+{
+    public static class Mock<T> where T : Component
+    {
+        public static T Create(string name)
+        {
+            var g = new GameObject(name + "_" + nameof(Mock<T>));
+            UnityObject.DontDestroyOnLoad(g);
+            g.transform.SetParent(Prefab.Parent.transform);
+            g.SetActive(false);
+
+            var mock = g.AddComponent<T>();
+            mock.name = Prefab.MockPrefix + name;
+
+            return mock;
+        }
+    }
+}
