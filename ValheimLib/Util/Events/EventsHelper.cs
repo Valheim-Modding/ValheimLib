57fd5dd41399f2ad15511235fee9a9351295ad8e
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-03-15 15:44:38 +0100

    null check in safe invoke

--- ValheimLib/Util/Events/EventsHelper.cs
+++ ValheimLib/Util/Events/EventsHelper.cs
@@ -21,6 +21,11 @@ namespace ValheimLib.Util.Events
 
         public static void SafeInvoke<TArg1>(this Action<TArg1> events, TArg1 arg1)
         {
+            if (events == null)
+            {
+                return;
+            }
+
             foreach (Action<TArg1> @event in events?.GetInvocationList())
             {
                 try
