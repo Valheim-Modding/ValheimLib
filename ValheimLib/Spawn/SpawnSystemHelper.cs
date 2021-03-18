11ef2240da80752db3807613b66f2fcc215e52c7
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-03-18 18:35:14 +0100

    link to the custom mob example

--- ValheimLib/Spawn/SpawnSystemHelper.cs
+++ ValheimLib/Spawn/SpawnSystemHelper.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using UnityEngine;
 using ValheimLib.Util.Events;
 
 namespace ValheimLib.Spawn
 {
+    /// <summary>
+    /// Highly recommend to check the example available <see href="https://github.com/Valheim-Modding/ExampleMod/blob/master/ExampleMod/Mobs/Example.cs">HERE</see>
+    /// </summary>
     public static class SpawnSystemHelper
     {
         internal static readonly List<SpawnSystem.SpawnData> CustomSpawnData = new List<SpawnSystem.SpawnData>();
 
         /// <summary>
         /// Event that get fired after the SpawnSystem get init for the first time.
+        /// An example on how it could be used is available  
+        /// <see href="https://github.com/Valheim-Modding/ExampleMod/blob/master/ExampleMod/Mobs/Example.cs">HERE</see>
         /// </summary>
         public static Action<SpawnSystem> OnAfterInit;
 
