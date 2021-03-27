d3234f915eb034c948d44b9ebc6ad67ec53b833f
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-03-27 02:52:38 +0100

    code cleanup

--- ValheimLib/Util/BoneReorder.cs
+++ ValheimLib/Util/BoneReorder.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-
 namespace ValheimLib.Util
 {
+    /// <summary>
+    /// Original code from https://github.com/GoldenJude
+    /// </summary>
     public static class BoneReorder
     {
         private static bool UtilityChanged = false,
@@ -29,6 +31,8 @@ namespace ValheimLib.Util
                     On.VisEquipment.SetHelmetEquiped += VisEquipmentOnSetHelmetEquiped;
                     On.VisEquipment.SetLegEquiped += VisEquipmentOnSetLegEquiped;
                 };
+
+                Applied = true;
             }
         }
 
@@ -38,7 +42,7 @@ namespace ValheimLib.Util
             if (changed) LegChanged = false;
             else LegChanged = true;
             var ret = orig(self, hash);
-            if(LegChanged && self.m_legItemInstances != null) SetSMRBones(self, hash, self.m_legItemInstances);
+            if (LegChanged && self.m_legItemInstances != null) ReorderBones(self, hash, self.m_legItemInstances);
             return ret;
         }
 
@@ -48,7 +52,8 @@ namespace ValheimLib.Util
             if (changed) HelmetChanged = false;
             else HelmetChanged = true;
             var ret = orig(self, hash, hairhash);
-            if(HelmetChanged && self.m_helmetItemInstance != null) SetSMRBones(self, hash, new List<GameObject>{self.m_helmetItemInstance}); //This is a single object instead of a collection, because reasons?
+            if (HelmetChanged && self.m_helmetItemInstance != null)
+                ReorderBones(self, hash, new List<GameObject>{self.m_helmetItemInstance}); //This is a single object instead of a collection, because reasons?
             return ret;
         }
 
@@ -58,7 +63,7 @@ namespace ValheimLib.Util
             if (changed) ChestChanged = false;
             else ChestChanged = true;
             var ret = orig(self, hash);
-            if(ChestChanged && self.m_chestItemInstances != null) SetSMRBones(self, hash, self.m_chestItemInstances);
+            if (ChestChanged && self.m_chestItemInstances != null) ReorderBones(self, hash, self.m_chestItemInstances);
             return ret;
         }
 
@@ -68,7 +73,7 @@ namespace ValheimLib.Util
             if (changed) ShoulderChanged = false;
             else ShoulderChanged = true;
             var ret = orig(self, hash, variant);
-            if(ShoulderChanged && self.m_shoulderItemInstances != null) SetSMRBones(self, hash, self.m_shoulderItemInstances);
+            if(ShoulderChanged && self.m_shoulderItemInstances != null) ReorderBones(self, hash, self.m_shoulderItemInstances);
             return ret;
         }
         
@@ -78,112 +83,106 @@ namespace ValheimLib.Util
             if (changed) UtilityChanged = false;
             else UtilityChanged = true;
             var ret = orig(self, hash);
-            if(UtilityChanged && self.m_utilityItemInstances != null) SetSMRBones(self, hash, self.m_utilityItemInstances);
+            if(UtilityChanged && self.m_utilityItemInstances != null) ReorderBones(self, hash, self.m_utilityItemInstances);
             return ret;
         }
-        
+
         /// <summary>
-        /// Reorders incorrect bone ordering caused by importing ripped assets into unity.
-        /// Code courtesy of: https://github.com/GoldenJude
+        /// Reorders bone ordering caused by importing ripped assets into unity.
+        /// It effectively matches the bone ordering from the ItemPrefab (itemPrefabHash parameter)
         /// </summary>
-        /// <param name="ve"></param>
-        /// <param name="hash"></param>
-        /// <param name="instances"></param>
-        private static void SetSMRBones(VisEquipment ve, int hash, List<GameObject> instances)
+        /// <param name="visEquipment"></param>
+        /// <param name="itemPrefabHash"></param>
+        /// <param name="instancesToFix">GameObjects that need to match the ordering from the ItemPrefab (itemPrefabHash parameter)</param>
+        private static void ReorderBones(VisEquipment visEquipment, int itemPrefabHash, List<GameObject> instancesToFix)
         {
-            Debug.Log($"SMRBones ran");
-                Transform skeletonRoot = ve.transform.Find("Visual").Find("Armature").Find("Hips");
-                GameObject itemPrefab = ObjectDB.instance.GetItemPrefab(hash);
-                if(!skeletonRoot) Debug.Log($"{skeletonRoot} is null.");
-                if(!itemPrefab) Debug.Log($"{itemPrefab} is null.");
-                int childCount = itemPrefab.transform.childCount;
-                int num = 0;
-                for (int i = 0; i < childCount; i++)
+            Log.LogInfo($"Reordering bones...");
+            Transform skeletonRoot = visEquipment.transform.Find("Visual").Find("Armature").Find("Hips");
+            GameObject itemPrefab = ObjectDB.instance.GetItemPrefab(itemPrefabHash);
+            if(!skeletonRoot) Log.LogInfo($"{skeletonRoot} is null.");
+            if(!itemPrefab) Log.LogInfo($"{itemPrefab} is null.");
+            int childCount = itemPrefab.transform.childCount;
+            int num = 0;
+            for (var i = 0; i < childCount; i++)
+            {
+                var itemPrefabChilds = itemPrefab.transform.GetChild(i);
+                if (itemPrefabChilds.name.StartsWith("attach_skin"))
                 {
-                    Transform child = itemPrefab.transform.GetChild(i);
-                    bool flag = child.name.StartsWith("attach_skin");
-                    if (flag)
+                    int j = 0;
+                    var meshRenderersToReorder = instancesToFix[num].GetComponentsInChildren<SkinnedMeshRenderer>();
+                    foreach (var meshRenderer in itemPrefabChilds.GetComponentsInChildren<SkinnedMeshRenderer>())
                     {
-                        int num2 = 0;
-                        // if(instances == null) Debug.Log($"instance collection was null.");
-                        // if(!instances[num]) Debug.Log($"instance was null.");
-                        // if(instances[num].GetComponentsInChildren<SkinnedMeshRenderer>() == null) Debug.Log($"SMR was null.");
-                        // Debug.Log($"{instances[num].GetComponentsInChildren<SkinnedMeshRenderer>()}");
-                         SkinnedMeshRenderer[] componentsInChildren = instances[num].GetComponentsInChildren<SkinnedMeshRenderer>();
-                         foreach (SkinnedMeshRenderer smr in child.GetComponentsInChildren<SkinnedMeshRenderer>())
-                         {
-                             SetBones(componentsInChildren[num2], GetBoneNames(smr), skeletonRoot);
-                             num2++;
-                         }
-                         num++;
+                        var meshRendererThatNeedFix = meshRenderersToReorder[j];
+                        meshRendererThatNeedFix.SetBones(meshRenderer.GetBoneNames(), skeletonRoot);
+                        j++;
                     }
+                    num++;
                 }
+            }
             
         }
-        
+
         /// <summary>
         /// Reorders incorrect bone ordering caused by importing ripped assets into unity.
-        /// Code courtesy of: https://github.com/GoldenJude
         /// </summary>
-        /// <param name="ve"></param>
-        /// <param name="hash"></param>
-        /// <param name="instances"></param>
-        private static void SetBones(SkinnedMeshRenderer smr, string[] boneNames, Transform skeletonRoot)
+        /// <param name="skinnedMeshRenderer"></param>
+        /// <param name="boneNames"></param>
+        /// <param name="skeletonRoot"></param>
+        private static void SetBones(this SkinnedMeshRenderer skinnedMeshRenderer, string[] boneNames, Transform skeletonRoot)
         {
-            Transform[] array = new Transform[smr.bones.Length];
-            for (int i = 0; i < array.Length; i++)
+            var bones = new Transform[skinnedMeshRenderer.bones.Length];
+            for (int i = 0; i < bones.Length; i++)
             {
-                array[i] = _findInChildren(skeletonRoot, boneNames[i]);
+                bones[i] = FindInChildren(skeletonRoot, boneNames[i]);
             }
-            smr.bones = array;
-            smr.rootBone = skeletonRoot;
+
+            skinnedMeshRenderer.bones = bones;
+            skinnedMeshRenderer.rootBone = skeletonRoot;
         }
-        
+
         /// <summary>
         /// Returns a list of bone names, given a SkinnedMeshRenderer
-        /// Code courtesy of: https://github.com/GoldenJude
         /// </summary>
-        /// <param name="ve"></param>
-        /// <param name="hash"></param>
-        /// <param name="instances"></param>
-        private static string[] GetBoneNames(SkinnedMeshRenderer smr)
+        /// <param name="skinnedMeshRenderer"></param>
+        /// <returns></returns>
+        private static string[] GetBoneNames(this SkinnedMeshRenderer skinnedMeshRenderer)
         {
-            List<string> list = new List<string>();
-            foreach (Transform transform in smr.bones)
+            var list = new List<string>();
+            foreach (var transform in skinnedMeshRenderer.bones)
             {
                 list.Add(transform.name);
             }
+
             return list.ToArray();
         }
-        
+
         /// <summary>
         /// Returns a transform matching the given name within the transforms children.
-        /// Code courtesy of: https://github.com/GoldenJude
         /// </summary>
-        /// <param name="ve"></param>
-        /// <param name="hash"></param>
-        /// <param name="instances"></param>
-        private static Transform _findInChildren(Transform trans, string name)
+        /// <param name="transform"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        private static Transform FindInChildren(Transform transform, string name)
         {
-            bool flag = trans.name == name;
             Transform result;
-            if (flag)
+            if (transform.name == name)
             {
-                result = trans;
+                result = transform;
             }
             else
             {
-                for (int i = 0; i < trans.childCount; i++)
+                for (int i = 0; i < transform.childCount; i++)
                 {
-                    Transform transform = _findInChildren(trans.GetChild(i), name);
-                    bool flag2 = transform != null;
-                    if (flag2)
+                    var childTransform = FindInChildren(transform.GetChild(i), name);
+                    if (childTransform != null)
                     {
-                        return transform;
+                        return childTransform;
                     }
                 }
+
                 result = null;
             }
+
             return result;
         }
     }
